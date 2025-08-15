9d2f05d28efe83f2a22acd1fecb3cd021ed1376f
animenzo <animenzo941@gmail.com>
2025-08-16 00:01:04 +0530

    infinite scrolling ques

--- frontend/inf-scroll/src/App.jsx
+++ frontend/inf-scroll/src/App.jsx
@@ -1,33 +1,88 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import { useEffect } from 'react'
+import { useRef } from 'react'
+import { useCallback } from 'react'
+//https://jsonplaceholder.typicode.com/photos?_page=${index}&_limit=9
 function App() {
-  const [count, setCount] = useState(0)
+  const loaderRef = useRef(null)
+  const [images,setImages] = useState([])
+  const [loading,setLoading] = useState(false)
+  const [page,setPage] = useState(2)
+//first step data ko ftch kra aur store kiya
+ const fetchImages =async (index)=>{
+   try {
+    const url = `https://jsonplaceholder.typicode.com/photos?_page=${index}&_limit=9`
+    console.log(url);
+    
+    const result = await fetch(url)
+    const data = await result.json()
+    return data;
+
+  } catch (error) {
+    console.log("error:",error);
+    
+  }
+ }
+
+ const getData = useCallback(async()=>{
+  if(loading) return;
+  setLoading(true)
+  const data = await fetchImages(page)
+  setImages((prevImages)=>[...prevImages,...data])
+  setPage((prevPage)=>prevPage+1)
+  setTimeout(()=>{
+    setLoading(false)
+  },2000)
+ },[page,loading])
+ 
+  useEffect(()=>{
+    const observer = new IntersectionObserver((entries)=>{
+      const target = entries[0]
+      if(target.isIntersecting){
+        //cals next page
+        getData()
+      }
+    })
+    if(loaderRef.current){
+      observer.observe(loaderRef.current)
+    }
+
+    return ()=>{
+      if(loaderRef.current){
+        observer.unobserve(loaderRef.current)
+      }
+    }
+  },[getData])
+
+
+ //data ko first page pr hi dikhya
+ const fetchFirstPage = async()=>{
+  const data = await fetchImages(1)
+  setImages(data)
+ }
+
+ useEffect(()=>{
+   fetchFirstPage()
+ },[])
+
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div>
+         {
+          images?.map((image)=>(
+            <img key={image.id}  src={image.thumbnailUrl} alt={image.title} />
+          ))
+         }
+        </div>
+        <div ref={loaderRef}>
+          {
+            loading && <h2>Loading...</h2>
+          }
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
